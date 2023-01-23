c2c595eb8e5230640aaf9e1dbd6c1391fc4d850b
Grzegorz Łukaszewicz <grzesieklukaszewicz@gmail.com>
2023-01-23 11:43:24 +0100

    Initial structure

---
+++ FrontEndApp/src/components/LeagueStart.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p>league start</p>
+    </div>
+</template>
+
+<style scoped>
+    p {
+        font-size: medium;
+    }
+</style>
+
+<script lang="ts">
+import Vue from 'vue'
+</script>
